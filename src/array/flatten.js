1355ae895867894b121d698ccf572a3545befd00
gfyanghao <gfyanghao@gf.com.cn>
2020-07-27 16:44:39 +0800

    feat: flatten

---
+++ src/array/flatten.js
@@ -0,0 +1,36 @@
+function flatten(array, depth, result = []) {
+  let index = -1,
+    length = array.length;
+
+  while (++index < length) {
+    let value = array[index];
+    if (depth > 0 && Array.isArray(value)) {
+      if (depth > 1) {
+        // 递归
+        flatten(value, depth - 1, result);
+      } else {
+        // 连接数组
+        arrayPush(result, value);
+      }
+    } else {
+      // 赋值
+      result[result.length] = value;
+    }
+  }
+  return result;
+}
+
+function arrayPush(array, values) {
+  let index = -1,
+    length = values.length,
+    offset = array.length;
+
+  while (++index < length) {
+    array[offset + index] = values[index];
+  }
+
+  return array;
+}
+
+flatten([1, [2], [3, [4]]], 1)
+// [1, 2, 3, [4]]
